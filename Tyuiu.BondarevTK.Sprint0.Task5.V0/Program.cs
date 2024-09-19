eddc5bdaf2d176ec94dc4fbecacdaae08fce795e
BondarevTK <BondarevTK101@gmail.com>
2024-09-19 20:51:24 +0300

    Task5

---
+++ Tyuiu.BondarevTK.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BondarevTK.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.BondarevTK.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(5, 10));
+            Console.WriteLine("A - B = " + DataService.Subtraction(5, 10));
+            Console.WriteLine("A * B = " + DataService.Multiplication(5, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
