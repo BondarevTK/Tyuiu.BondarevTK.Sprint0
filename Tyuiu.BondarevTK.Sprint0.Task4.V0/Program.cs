8b45210c588244d7445f700171dc3a9cbf10ab8c
BondarevTK <BondarevTK101@gmail.com>
2024-09-19 20:30:06 +0300

    Task4

---
+++ Tyuiu.BondarevTK.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BondarevTK.Sprint0.Task4.V0.lib;
+namespace Tyuiu.BondarevTK.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 10));
+            Console.WriteLine(DataService.Subtraction(5, 10));
+            Console.WriteLine(DataService.Multiplication(5, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
